85cf3ffb2eb00025b83b99ba0f5dc0d31ec7dad0
Josh Hall-Bachner <jhallbachner@gmail.com>
2013-06-11 23:18:27 -0700

    Updated service definition to meet standards for Symfony bundles.

--- DependencyInjection/TedivmStashExtension.php
+++ DependencyInjection/TedivmStashExtension.php
@@ -22,8 +22,8 @@ class TedivmStashExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
 
         $processor = new Processor();
         $config = $processor->processConfiguration(new Configuration(), $configs);
