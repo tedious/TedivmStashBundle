6b93b2a333c35d8ef07fbd38d823cc54798344ce
Josh Hall-Bachner <jhallbachner@gmail.com>
2013-06-16 00:54:35 -0700

    Extended the SessionHandler to add a method clear all sessions.

--- Adapters/SessionHandlerAdapter.php
+++ Adapters/SessionHandlerAdapter.php
@@ -5,7 +5,26 @@ namespace Tedivm\StashBundle\Adapters;
 use Stash\Session;
 
 if(version_compare(phpversion(), '5.4.0', '>=')){
-    class SessionHandlerAdapter extends Session {}
+    class SessionHandlerAdapterShim extends Session {}
 } else {
-    class SessionHandlerAdapter extends Session implements \SessionHandlerInterface {}
+    class SessionHandlerAdapterShim extends Session implements \SessionHandlerInterface {}
+}
+
+class SessionHandlerAdapter extends SessionHandlerAdapterShim
+{
+    protected function getCache($session_id)
+    {
+        $path = 'ss_ss/' .
+            base64_encode($this->path) . '/' .
+            base64_encode($this->name) . '/' .
+            base64_encode($session_id);
+
+        return $this->pool->getItem($path);
+    }
+
+    public function clearAll()
+    {
+        $item = $this->pool->getItem('ss_ss');
+        $item->clear();
+    }
 }
