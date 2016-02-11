f33aa14129faa935fedcd67a2d70c779a022659a
Robert Hafner <tedivm@tedivm.com>
2016-02-10 16:53:20 -0800

    Added Stash API changes from v0.14.0

--- Adapters/DoctrineAdapter.php
+++ Adapters/DoctrineAdapter.php
@@ -107,9 +107,8 @@ class DoctrineAdapter implements DoctrineCacheInterface
 
         $id = $this->normalizeId($id);
         $item = $this->cacheService->getItem($id);
-
-        return $item->set($data, $lifeTime);
-
+        $item->set($data, $lifeTime);
+        return $this->cacheService->save($item);
     }
 
     /**
@@ -118,13 +117,12 @@ class DoctrineAdapter implements DoctrineCacheInterface
     public function delete($id)
     {
         $id = $this->normalizeId($id);
-
-        return $this->cacheService->clear($id);
+        return $this->cacheService->deleteItem($id);
     }
 
     public function deleteAll()
     {
-        return $this->flushAll();
+        return $this->delete('');
     }
 
     /**
@@ -165,7 +163,7 @@ class DoctrineAdapter implements DoctrineCacheInterface
     protected function normalizeId($id)
     {
         $namespace = (isset($this->namespace) && $this->namespace != '') ? $this->namespace : 'default';
-        $id = sprintf('zz_%s_zz/%s', $namespace, $id);
+        $id = sprintf('/zz_%s_zz/%s', $namespace, $id);
 
         return $id;
     }
